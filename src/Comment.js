0da5df9872cc9fb854ddd0450bbfde0b84aed61c
Dat Nguyen <dnguyenk2@gmail.com>
2022-05-29 19:09:47 -0700

    Completed Assignment

--- src/Comment.js
+++ src/Comment.js
@@ -4,4 +4,8 @@ function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
 
+/* can use arrow function instead
+const Comment = () => {return< div>Naturally, I agree with this article.</div>}; 
+*/
+
 export default Comment;
